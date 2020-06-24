5ed0c09a4d1159b5a217dacf0c7359f373cd9f17
Dell <Dell@DESKTOP-09A0ULN>
2020-06-24 20:11:42 +0530

    why

--- firstpro/src/main/java/com/apex/firstpro/App.java
+++ firstpro/src/main/java/com/apex/firstpro/App.java
@@ -9,7 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
-     System.out.println( "Hello World!" );
+     System.out.println( "ello World!" );
     sim s=new airtel();
     s.data();
   //  	ApplicationContext context=new ClassPathXmlApplicationContext();

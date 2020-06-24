2f261ee85db74737dba0cbf6aabf8f7ce998779b
Dell <Dell@DESKTOP-09A0ULN>
2020-06-24 19:20:33 +0530

    new

--- firstpro/src/main/java/com/apex/firstpro/App.java
+++ firstpro/src/main/java/com/apex/firstpro/App.java
@@ -4,10 +4,15 @@ package com.apex.firstpro;
  * Hello world!
  *
  */
+//import org.springframework.context.ApplicationContext;
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    //    System.out.println( "Hello World!" );
+    sim s=new airtel();
+    s.data();
+  //  	ApplicationContext context=new ClassPathXmlApplicationContext();
+    	
     }
 }
